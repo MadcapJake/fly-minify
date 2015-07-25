6363ef9999f0ce7dfed90d58cc086ca801212ac3
Jake Russo <madcap.russo@gmail.com>
2015-07-25 00:39:51 -0500

    Working implementation :joy:

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
+const assign = require("object-assign")
+const minifier = require("minify")
 module.exports = function () {
   const minify = (src, opts, cb) => {
-    require("minify")(opts, (err, data) =>
-      err ? (() => { throw err })() : cb(data))
+    minifier(assign(opts, { data: src }), function (err, data) {
+      if (err) throw err
+      cb(null, data)
+    })
   }
 
-  this.filter("minify", (source, options) =>
-    this.defer(minify)(source, options))
+  this.filter("minify", (source, options) => {
+    if (!options.ext) throw new TypeError("Minify options must contain an extension")
+    if (options.ext[0] !== ".") options.ext = "." + options.ext
+    return this.defer(minify)(source, options)
+  })
 }
